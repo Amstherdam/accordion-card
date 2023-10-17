34258ef149690482ec94792e76b9fe796dde847d
Amstherdam <developer.amst@gmail.com>
2023-10-18 00:19:58 +0300

    fixed some toggle problem

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const visibleItemContainer = document.querySelectorAll('.acordionItemContainer')
 const rotateArrow = document.querySelectorAll('.rotateArrow')
 
-// const openOfItem = document.querySelectorAll('.acordionBottomItem')
+
 
 
 
@@ -10,15 +10,16 @@ visibleItemContainer.forEach((item) => {
 
     let openTextItem = item.childNodes[3]
     let rotateArrowItem =  item.childNodes[1].childNodes[3]
-
+    let bolderQuestion = item.childNodes[1]
 
 
     item.addEventListener('click', () => { 
     
          openTextItem.classList.toggle('visible')
          rotateArrowItem.classList.toggle('rodtate')
-
-         console.log(openTextItem)
+         bolderQuestion.classList.toggle('bold')
+         
+         
     })
 
 
